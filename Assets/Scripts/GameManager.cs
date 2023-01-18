f5f258d9af53bf7804c4ae622d1c71d0ba463123
Diego Camilo Herrera Ferrer <danthe1996@gmail.com>
2023-01-18 17:24:24 -0500

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//estados del juego 
+public enum GameState
+{
+    menu,
+    inTheGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    //permite compartir estado del juego entre componentes
+    public static GameManager sharedInstance;
+
+    //Se inicia el estado del juego en el menu principal
+    public GameState currentGameState = GameState.menu;
+
+    //se ejecuta a iniciar el programa
+    void Awake()
+    {
+        //selecciona la instancia creada
+        sharedInstance = this;    
+    }
+
+    void Start()
+    {
+        //Arranca el manager en el estado menu
+        currentGameState = GameState.menu;
+    }
+
+    void Update()
+    {
+        //Si presionamos la tecla p cambiamos estado de menu a estado de juego
+        if (Input.GetButtonDown ("p"))
+        {
+            currentGameState = GameState.inTheGame;
+        }
+        if (Input.GetButtonDown("Escape"))
+        {
+            currentGameState = GameState.menu;
+        }
+    }
+
+    //Se llama para iniciar la partida
+    public void StartGame()
+    {
+        //Inicia el estado del juego a in game
+        ChangeGameState(GameState.inTheGame);
+    }
+
+    //Se llama cuando el jugador pierde
+    public void GameOver()
+    {
+        ChangeGameState(GameState.gameOver);
+    }
+
+    //se llama cuando se quiera regresar al menu
+    public void BackToMainMenu()
+    {
+        ChangeGameState(GameState.menu);
+    }
+
+    //metodo encargado de cambiar los estados
+    void ChangeGameState(GameState newGameState)
+    {
+        if (newGameState == GameState.menu)
+        {
+
+        }
+        else if (newGameState == GameState.inTheGame)
+        {
+
+        }
+        else if (newGameState == GameState.gameOver)
+        {
+
+        }
+
+        currentGameState = newGameState;
+    }
+
+}
